fa96864799e86c3224f85804cbba79308770a4ff
dekompot <dekompot@gmail.com>
2023-02-01 00:12:09 +0100

    Stork and Finish

--- Stork.scala
+++ Stork.scala
@@ -6,28 +6,31 @@ import akka.actor.typed.scaladsl.Behaviors
 
 import scala.util.Random
 
-object ProgrammingParadigms {
+object Stork {
 
-  private final val SLOTS_NUMBER = 3
-  private final val TIME_IN_MILISEC = 10368000f
-  private final val SUCCESS_PROBABILITY = 0.5
+  private final val SLOTS_NUMBER = 1
+  private final val TIME_IN_MILISEC = 500f
+  private final val SUCCESS_PROBABILITY = 0.99
   private var slotsTaken = 0
 
-  def apply(): Behavior[Produce] = Behaviors.receive { (context, message) =>
+  def apply(next: ActorRef[Produce]): Behavior[Produce] = Behaviors.receive
+  { (context, message) =>
     message match {
       case ProduceOneInstance(barn) =>
         slotsTaken = slotsTaken + 1
         Thread sleep ((TIME_IN_MILISEC + TIME_IN_MILISEC * Random.nextFloat) / Barn.getSpeed).toLong
         if ((Random.nextFloat()) <= SUCCESS_PROBABILITY)
-          barn ! Barn.AddStudent()
-        context.log.info("pp asks barn for resources")
+          barn ! Barn.AddPreBaby(next)
+        context.log.info("school asks barn for resources")
         slotsTaken = slotsTaken - 1
-        barn ! Barn.AskForStudentResources(context.self)
+        barn ! Barn.AskForPreBabyResources(context.self)
         Behaviors.same
       case AskForAvailableSlot(barn) =>
         if (slotsTaken < SLOTS_NUMBER)
-          barn ! Barn.AskForStudentResources(context.self)
+          barn ! Barn.AskForPreBabyResources(context.self)
         Behaviors.same
     }
+
   }
+
 }
